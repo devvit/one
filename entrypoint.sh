554ef389d7cb02c77ae390d3cb82790a1dd67183
devvit <you@example.com>
2019-05-19 12:46:03 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# gcp: us-central1/europe-west1
+# gcp: v2018-us-central1/europe-west1
 # aws: us-east-1/us-west-2
 # gigalixir create -n nov28 --cloud aws --region us-west-2
 
