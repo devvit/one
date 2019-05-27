87eee45b9f0eb2a3972be65ce1c8e07654776b8b
devvit <you@example.com>
2019-05-27 23:43:39 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -68,7 +68,7 @@ honcho start -f Procfile.honcho
 
 # gcp: v2018-us-central1/europe-west1
 # aws: us-east-1/us-west-2
-# gigalixir create -n nov28 --cloud aws --region us-west-2
+# gigalixir create -n APP_NAME --cloud aws --region us-west-2
 
 # docker tag <image> registry.heroku.com/<app>/web
 # docker push registry.heroku.com/<app>/<process-type>
@@ -79,3 +79,7 @@ honcho start -f Procfile.honcho
 # docker system prune -a
 
 # steganographer inputImage.png -f fileToHide.zip
+
+######
+######
+######
