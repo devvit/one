ef56a336b79e8295567f399caab97b0611870514
devvit <you@example.com>
2019-06-07 18:19:55 +0800

    update

--- myapp.py
+++ myapp.py
@@ -19,7 +19,7 @@ device = os.getenv('DEVICE') or 'lo'
 @routes.get('/')
 @aiohttp_jinja2.template('index.html')
 async def hello(request):
-    return dict(ver='0606')
+    return dict(ver='0101')
 
 
 async def websocket_handler(request):
