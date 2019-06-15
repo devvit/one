0b99b5f8abf1bc3fa3fa432727f799207d32f8b2
devvit <you@example.com>
2019-06-15 18:35:18 +0800

    update

--- myapp.py
+++ myapp.py
@@ -11,9 +11,14 @@ import jinja2
 import psutil
 
 from aiohttp import web
+from aiohttp_sse import sse_response
 
 routes = web.RouteTableDef()
 device = os.getenv('DEVICE') or 'lo'
+
+
+async def on_prepare(request, response):
+    print(request.headers)
 
 
 @routes.get('/')
@@ -22,29 +27,39 @@ async def hello(request):
     return dict(ver='0101')
 
 
-async def websocket_handler(request):
+async def sse_test(request):
+    # loop = request.app.loop
+    async with sse_response(request) as resp:
+        while True:
+            s0 = psutil.net_io_counters(pernic=True)[device].bytes_recv
+            await asyncio.sleep(1)
+            s1 = psutil.net_io_counters(pernic=True)[device].bytes_recv
+            s3 = s1 - s0
+            now = int(time.time())
+            await resp.send(json.dumps([
+                dict(time=now, y=s3),
+                dict(time=now, y=s1),
+                dict(time=now, y=psutil.cpu_percent()),
+                dict(time=now, y=psutil.virtual_memory().percent)
+            ]))
+    return resp
+
+
+async def ws_test(request):
     ws = web.WebSocketResponse()
     await ws.prepare(request)
     while True:
-        s0 = psutil.net_io_counters(pernic=True)[device].bytes_recv
-        await asyncio.sleep(1)
-        s1 = psutil.net_io_counters(pernic=True)[device].bytes_recv
-        s3 = s1 - s0
-        now = int(time.time())
-        await ws.send_str(json.dumps([
-            dict(time=now, y=s3),
-            dict(time=now, y=s1),
-            dict(time=now, y=psutil.cpu_percent()),
-            dict(time=now, y=psutil.virtual_memory().percent)
-        ]))
+        pass
 
     return ws
 
 
 app = web.Application()
+app.on_response_prepare.append(on_prepare)
 # app.add_routes([web.static('/static', 'static')])
 aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('templates'))
-app.add_routes([web.get('/test', websocket_handler)])
+app.router.add_route('GET', '/sse_test', sse_test)
+# app.add_routes([web.get('/ws_test', ws_test)])
 app.add_routes(routes)
 
 if __name__ == '__main__':
